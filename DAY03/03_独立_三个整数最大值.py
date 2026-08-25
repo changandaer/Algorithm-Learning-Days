a48fb7d142c9abcd34b132c05d75e55e5b02c743
CCY <changandaerduo@gmail.com>
2026-08-25 21:00:13 +0800

    DAY02 Save4th

---
+++ DAY03/03_独立_三个整数最大值.py
@@ -0,0 +1,12 @@
+"""DAY03必做二：不使用max()，找出三个整数中的最大值。"""
+
+# TODO 1：分别读取三个整数。
+
+# TODO 2：先把第一个数保存为current_max（当前最大值）。
+
+# TODO 3：如果第二个数更大，更新current_max。
+
+# TODO 4：如果第三个数更大，再更新current_max。
+
+# TODO 5：输出current_max，并完成课程要求的4组测试。
+
