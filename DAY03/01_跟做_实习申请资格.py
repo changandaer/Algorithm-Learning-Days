9483fc4f94f815c049906beae7343a71fda7864a
CCY <changandaerduo@gmail.com>
2026-08-25 21:58:12 +0800

    手敲跟做

--- DAY03/01_跟做_实习申请资格.py
+++ DAY03/01_跟做_实习申请资格.py
@@ -5,10 +5,42 @@
 """
 
 # TODO 1：接收可实习月数、每周小时数和是否学过Python。
+month = int(input("可实习月数："))
+hour = int(input("每周可实习小时数："))
+know_python = input("是否学过python：")
 
 # TODO 2：把Y/N回答转换成布尔值。
+if month > 3:
+    month = True
+else:
+    month = False
+
+if hour > 40:
+    hour = True
+else:
+    hour = False
+
+if know_python == "是":
+    know_python = True
+elif know_python == "否":
+    know_python = False
+else:
+    know_python = False
+    print("请重新回答是否学过python")
 
 # TODO 3：使用and得到是否满足申请条件。
-
 # TODO 4：输出结果；不满足时分别输出所有真实原因。
+if month and hour and know_python:
+    print("满足申请条件")
+else:
+    print("不满足申请条件")
+
+    if not month:
+        print("实习月数不满足")
+    if not hour:
+        print("每周可实习小时数不满足")
+    if not know_python:
+        print("不会python")
+
+
 
