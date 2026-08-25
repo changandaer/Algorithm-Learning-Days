21acc2a6cbb1c24453043be344bddd0af467f7bc
CCY <changandaerduo@gmail.com>
2026-08-25 19:39:07 +0800

    DAY02 Save1st

--- DAY02/01_今天要做/今日代码/02_独立_比较两个整数.py
+++ DAY02/01_今天要做/今日代码/02_独立_比较两个整数.py
@@ -3,8 +3,13 @@
 不要复制完整答案。完成后依次测试：(8, 3)、(2, 7)、(5, 5)、(-2, -5)。
 """
 
-first_number = 8
-second_number = 3
+first_number = -2
+second_number = -5
 
 # TODO：使用if、elif、else覆盖“第一个更大、第二个更大、相等”三种情况。
-
+if first_number > second_number:
+    print(f"较大的数是：{first_number}")
+elif first_number < second_number:
+    print(f"较大的数是"{second_number})
+else:
+    print("两个数相等")
