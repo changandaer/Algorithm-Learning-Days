ffe807b8182e6213ea62ea569d81d904e3615b76
CCY <changandaerduo@gmail.com>
2026-08-26 10:37:09 +0800

    手敲for循环求和

--- DAY04/01_跟做_for循环求和.py
+++ DAY04/01_跟做_for循环求和.py
@@ -5,10 +5,18 @@
 """
 
 # TODO 1：读取正整数 n。
+n = int(input("输入一个正整数："))
 
 # TODO 2：如果 n 不合法，输出提示。
+if n < 0 or n == 0:
+    print("数据不合法，重新输入")
 
 # TODO 3：否则使用 for、range 和累计器求和。
+else:
+    total = 0
+    for i in range(1, n+1):
+        total += i
 
 # TODO 4：输出结果，并测试 n 为 1、5、10。
+    print(f"1到n的整数和为{total}")
 
