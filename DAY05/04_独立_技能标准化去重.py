ede55adcd4b6255e3e16a76c04d12637dbf5dcb0
CCY <changandaerduo@gmail.com>
2026-08-26 20:21:27 +0800

    手敲去重技能

--- DAY05/04_独立_技能标准化去重.py
+++ DAY05/04_独立_技能标准化去重.py
@@ -3,11 +3,38 @@
 限制：今天不使用 set。
 """
 
-# TODO 1：读取一行技能，使用 split() 得到原始列表。
+# 测试 split() 函数的用法
 
-# TODO 2：建立空的去重结果列表。
+# text = "python  linux      git"
+# skills = text.split()
+# print(skills)
 
-# TODO 3：遍历、转小写，只追加尚未出现的技能。
+# text = "python，    linux，git"
+# skills = text.split()
+# print(skills)
 
-# TODO 4：输出去重列表和数量。
+# text = "python,linux,git"
+# skills = text.split()
+# print(skills)
+
+skills = input("输入你的技能：").strip().lower().split()
+# skills = input("输入你的技能：").split().strip().lower()会报错
+# 因为split()提前将字符串转换为了列表
+# 在 Python 中 strip() 和 lower() 这两个方法基本上只能作用于字符串对象而不能是列表
+
+deduplicate_skills = []
+num = 0
+for index in range(len(skills)):
+    if skills[index] in deduplicate_skills: 
+        print(f"{skills[index]}技能已经存在")
+    else:
+        deduplicate_skills.append(skills[index])
+        num += 1
+
+print(f"去重之后有{num}项技能，分别是{deduplicate_skills}")
+
+
+
+
+
 
