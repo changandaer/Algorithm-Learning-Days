1b9eb18da5656007329974f9ff095e7e31fad4e1
CCY <changandaerduo@gmail.com>
2026-08-26 10:52:14 +0800

    手敲FizzBuzz

--- DAY04/03_独立_FizzBuzz.py
+++ DAY04/03_独立_FizzBuzz.py
@@ -3,9 +3,15 @@
 15 和 30 必须输出 FizzBuzz，而不是 Fizz 或 Buzz。
 """
 
-# TODO 1：使用 for 和 range 遍历 1～30。
 
-# TODO 2：按正确顺序判断同时整除、只整除 3、只整除 5、其他。
+for i in range(1, 31):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
 
-# TODO 3：运行后重点检查 3、5、15、30。
+
+
 
