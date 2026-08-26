218fb8f387bf8e12ac958185e78e48fb27c6c881
CCY <changandaerduo@gmail.com>
2026-08-26 10:47:19 +0800

    手敲成绩输入验证

--- DAY04/02_独立_成绩输入验证.py
+++ DAY04/02_独立_成绩输入验证.py
@@ -4,8 +4,19 @@
 """
 
 # TODO 1：第一次读取成绩。
+score = int(input("输入你的成绩："))
 
 # TODO 2：成绩不在 0～100 时，使用 while 反复读取。
+while score < 0 or score > 100:
+    score = int(input("成绩不合法，重新输入你的成绩："))
 
 # TODO 3：合法后，使用 if/elif/else 输出 A、B、C 或 D。
+if score >= 90:
+    print("A")
+elif score >=80:
+    print("B")
+elif score >=60:
+    print("C")
+else:
+    print("D")
 
