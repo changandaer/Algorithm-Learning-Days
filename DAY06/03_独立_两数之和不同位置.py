8715e561ecc07220a15a2fffcc654644347db35c
CCY <changandaerduo@gmail.com>
2026-08-27 18:43:59 +0800

    手敲两数之和

--- DAY06/03_独立_两数之和不同位置.py
+++ DAY06/03_独立_两数之和不同位置.py
@@ -4,17 +4,36 @@
 """
 
 numbers = [2, 7, 11, 15]
-target = 9
+target = 100
 
 # 我的拆题
-# 输入：
-# 输出：
-# 最小样例的手算过程：
-# 必须记住的状态：
+# 输入：容器为列表，元素为正整数
+# 输出：索引、数值
+# 最小样例的手算过程：numbers = [2, 2]，index=0
+# 必须记住的状态：是否存在这两个数，存在的话在那个位置数值是多少
 # 重复动作与控制结构：
 # 普通测试、边界测试、反例：
 # 中文伪代码：
-
+# 双重循环遍历列表
+#   判断是否有两个数相加等于目标值
+#   如果有跳出循环
+#   如果没有输出没有
 
 # 从这里开始独立实现。不要照抄课程中的示范答案。
 
+is_equal = False
+
+for i in range(len(numbers)):
+    for j in range(1,len(numbers)):
+        if numbers[i] + numbers[j] == target:
+            is_equal = True
+            break
+        else:
+            pass
+    if is_equal:
+        break
+if is_equal:
+    print(f"第{i+1}个数字与第{j+1}个数字之和等于目标数字{target}")
+else:
+    print(f"没找到两个数字之和等于目标数字{target}")
+        
