7482190f7d0739a4da75d2aeb5ac235d0810571d
CCY <changandaerduo@gmail.com>
2026-08-28 23:20:35 +0800

    手敲空输入与列表去重

--- DAY07/01_修复_空输入与列表去重.py
+++ DAY07/01_修复_空输入与列表去重.py
@@ -3,10 +3,28 @@
 限制：不使用 set，不对空输入进行第二次询问。
 """
 
-# 最小样例：
+# 最小样例：python linux git
 # 处理阶段：
-# 必须记住的状态：
+# 判断输入是否合法
+#   输入为空，直接输出空列表
+#   输入不为空，进行标准化并去重
+# 必须记住的状态：输入标准化后的列表，去重元素，去重列表
 
 
 # 从这里开始实现，并输出结果列表和数量。
 
+
+job_skills = input("输入你的技能，用空格隔开：").strip().lower().split()
+# print(job_skills)
+
+unique_skills = []
+if job_skills == []:
+    print("输入为空，数量为0")
+else:
+    for unique_skill in job_skills:
+        if unique_skill in unique_skills:
+            pass
+        else:
+            unique_skills.append(unique_skill)
+
+print(f"{unique_skills}")
