637578b0b90ab1a6bc4601124dd089a85c1b2092
CCY <changandaerduo@gmail.com>
2026-08-28 23:31:09 +0800

    统计单个技能次数

--- DAY07/03_跟做_统计单个技能次数.py
+++ DAY07/03_跟做_统计单个技能次数.py
@@ -7,9 +7,14 @@ all_skills = [
 ]
 target_skill = "python"
 
-# 先手算 target_skill 的预期次数：
-# 程序必须记住的状态：
+# 先手算 target_skill 的预期次数：3
+# 程序必须记住的状态：次数
 
 
 # 从这里开始实现。只需要一层循环和一个计数器。
+count = 0
+for skill in all_skills:
+    if skill == target_skill:
+        count += 1
 
+print(f"目标技能{target_skill}出现了{count}次")
