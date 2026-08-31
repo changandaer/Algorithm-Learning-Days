202fb90f1b692340e56b746b06f0fae45c3e142f
CCY <changandaerduo@gmail.com>
2026-08-31 17:23:40 +0800

    两数之和

--- DAY09/03_修复_两数之和参数.py
+++ DAY09/03_修复_两数之和参数.py
@@ -2,13 +2,25 @@
 
 # 函数合同
 # 函数名：find_two_sum
-# 参数：
-# 返回值及类型：
-# 最小例子：
+# 参数：numbers、target
+# 返回值及类型：返回索引列表
+# 最小例子：([2, 7, 11, 15], 9) -> [0, 1]
 
 
 # 定义函数。
-
+def find_two_sum(numbers,target):
+    target_index = []
+    for first_index in range(len(numbers)):
+        for second_index in range(first_index+1,len(numbers)):
+            if numbers[first_index] + numbers[second_index] == target:
+                target_index.append(first_index)
+                target_index.append(second_index)
+    return target_index
 
 # 测试固定样例、[3,3]、空列表、无答案和存在多组答案的数据。
 
+assert find_two_sum([2, 7, 11, 15], 9) == [0, 1]
+assert find_two_sum([2, 7, 11, 15], 26) == [2, 3]
+assert find_two_sum([2, 7, 11, 15], 14) == []
+assert find_two_sum([3, 3], 6) == [0, 1]
+assert find_two_sum([], 9) == []
