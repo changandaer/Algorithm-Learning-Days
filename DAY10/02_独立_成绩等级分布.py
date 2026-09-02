e6520f7cd671bb8fb21c8b5c24ebbcd668937c1d
CCY <changandaerduo@gmail.com>
2026-09-02 12:35:11 +0800

    成绩等级分布

--- DAY10/02_独立_成绩等级分布.py
+++ DAY10/02_独立_成绩等级分布.py
@@ -8,4 +8,23 @@ scores = [95, 82, 60, 59, 101, 90, 82]
 
 
 # 定义get_grade，再统计每个返回等级出现的次数。
+def get_grade(scores):
+    score_level = {}
+    for score in scores:
+        if score < 0 or score > 100:
+            pass
+        else:
+            if score >= 90:
+                score_level["A"] = score_level.get("A",0) + 1
+            if score >= 80:
+                score_level["B"] = score_level.get("B",0) + 1
+            if score >= 60:
+                score_level["C"] = score_level.get("C",0) + 1
+            else:
+                score_level["D"] = score_level.get("D",0) + 1
+    return score_level
+
+score_level = get_grade(scores)
+print(score_level)
+
 
