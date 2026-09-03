eb3f783655c7eeba434f6b9534653e44f699c7d5
CCY <changandaerduo@gmail.com>
2026-09-03 18:32:52 +0800

    两数之和

--- DAY11/01_跟做_两数之和暴力法.py
+++ DAY11/01_跟做_两数之和暴力法.py
@@ -1,6 +1,6 @@
 """DAY11跟做：使用两层循环完成两数之和。"""
 
-# 第一步：最小输入与预期输出：
+# 第一步：最小输入与预期输出：[2,7,11,15]/9 -> [0, 1]
 # 第二步：代码阶段：
 # 第三步：需要保存的状态与小测试：
 
@@ -10,3 +10,16 @@
 
 # 测试：[2,7,11,15]/9，[3,2,4]/6，[3,3]/6。
 
+def two_sum_brute_force(nums, target):
+
+    seen = {}
+    for index in range(len(nums)):
+        needed = target - nums[index]
+        if needed in seen:
+            return [seen[needed],index]
+        seen[nums[index]] = index
+    return []
+
+assert two_sum_brute_force([2,11,15,7],9) == [0,3]
+assert two_sum_brute_force([3,2,4],6) == [1, 2]
+assert two_sum_brute_force([3,3],6) == [0,1]
