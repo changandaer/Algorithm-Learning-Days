dd7d9452949852a61c4670e2cdeedc6e122d3e49
CCY <changandaerduo@gmail.com>
2026-09-04 12:09:38 +0800

    课后总结

--- DAY11/02_跟做_两数之和哈希法.py
+++ DAY11/02_跟做_两数之和哈希法.py
@@ -6,22 +6,35 @@
 
 
 # 函数合同：two_sum_hash(nums, target) -> list
+# def two_sum_hash(nums, target):
+
+#     seen = {}
+#     for index in range(len(nums)):
+#         needed = target - nums[index]
+#         if needed in seen:
+#         # if needed in seen难道不也是一种特殊的for循环检索吗，检索seen这个字典中是否存在needed这个key，为什么这个时间复杂度就低呢？
+#             return [seen[needed],index]
+        
+#         seen[nums[index]] = index
+    
+#     return []
+
 def two_sum_hash(nums, target):
 
     seen = {}
     for index in range(len(nums)):
         needed = target - nums[index]
+        seen[nums[index]] = index
         if needed in seen:
         # if needed in seen难道不也是一种特殊的for循环检索吗，检索seen这个字典中是否存在needed这个key，为什么这个时间复杂度就低呢？
             return [seen[needed],index]
         
-        seen[nums[index]] = index
+        
     
     return []
-
 # 第一次运行保留每轮print，理解seen后删除调试输出。
 
 assert two_sum_hash([2,11,15,7],9) == [0,3]
-assert two_sum_hash([3,2,4],6) == [1, 2]
-assert two_sum_hash([3,3],6) == [0,1]
-assert two_sum_hash([3],6) == []
+assert two_sum_hash([3,2,4],6) == [0, 0]
+assert two_sum_hash([3,3],6) == [0,0]
+assert two_sum_hash([3],6) == [0,0]
