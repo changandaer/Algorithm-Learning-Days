db044de7a8d2bff910fc2c3d6f6277abaa61589e
CCY <changandaerduo@gmail.com>
2026-09-09 11:50:46 +0800

    逐行读取

--- DAY13/02_独立_逐行读取有效岗位.py
+++ DAY13/02_独立_逐行读取有效岗位.py
@@ -1,1 +1,6 @@
 """DAY13任务2：要求见课程文档。请自己写三步分析，再从空白完成实现和测试。"""
+
+with open("DAY13/岗位技能样例.txt","r",encoding="utf-8") as file:
+    for file_line in file:
+        clean_line = file_line.strip()
+        print(clean_line)
