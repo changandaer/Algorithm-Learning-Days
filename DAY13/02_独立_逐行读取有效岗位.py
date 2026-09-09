776b65af5f055812465c8a288205230039845597
CCY <changandaerduo@gmail.com>
2026-09-09 12:14:11 +0800

    逐行读取

--- DAY13/02_独立_逐行读取有效岗位.py
+++ DAY13/02_独立_逐行读取有效岗位.py
@@ -1,6 +1,16 @@
 """DAY13任务2：要求见课程文档。请自己写三步分析，再从空白完成实现和测试。"""
 
-with open("DAY13/岗位技能样例.txt","r",encoding="utf-8") as file:
-    for file_line in file:
-        clean_line = file_line.strip()
-        print(clean_line)
+
+def load_job_lines(file_path):
+
+    with open(file_path,"r",encoding="utf-8") as file:
+        job_skills = []
+        for file_line in file:
+            clean_line = file_line.strip()
+            if clean_line != '':
+                job_skills.append(clean_line)
+    return job_skills
+    
+job_skills = load_job_lines("DAY13/岗位技能样例.txt")
+print(job_skills)
+            
