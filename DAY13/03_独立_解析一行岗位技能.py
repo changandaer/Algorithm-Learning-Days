de0881391a21136f443c6aa425cdd5d851d98bd2
CCY <changandaerduo@gmail.com>
2026-09-09 12:32:37 +0800

    解析一行

--- DAY13/03_独立_解析一行岗位技能.py
+++ DAY13/03_独立_解析一行岗位技能.py
@@ -1,1 +1,20 @@
 """DAY13任务3：要求见课程文档。请自己写三步分析，再从空白完成实现和测试。"""
+
+
+
+def parse_skill_line(line):
+
+    normal_skills = []
+
+    job_skills = line.split(',')
+
+    for job_skill in job_skills:
+        normal_skill = job_skill.strip().lower()
+
+        normal_skills.append(normal_skill)
+
+    return normal_skills
+
+
+normal_skills = parse_skill_line(" Python, C++, Linux, Git ")
+print(normal_skills)
