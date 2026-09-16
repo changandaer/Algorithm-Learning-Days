cb51549db884b7e3e58581ebb7373a8e635cdd34
CCY <changandaerduo@gmail.com>
2026-09-16 11:29:33 +0800

    LC两数之和

--- DAY18/01_Python复习_LeetCode1_两数之和.py
+++ DAY18/01_Python复习_LeetCode1_两数之和.py
@@ -1,1 +1,26 @@
 """DAY18 Python复习：闭卷重写两数之和；要求见当天课程，自己写三步分析、实现与测试。"""
+
+class Solution:
+
+    def twoSum(self, nums, target):
+
+        seen = {}
+
+        for index in range(len(nums)):
+
+            needed = target - nums[index]
+
+            if needed in seen:
+                
+                return [seen[needed],index]
+            
+            seen[nums[index]] = index
+        
+        return []
+
+solution = Solution()
+assert solution.twoSum([2, 7, 11, 15],9) == [0, 1]
+assert solution.twoSum([3, 2, 4],6) == [1, 2]
+assert solution.twoSum([3, 3],6) == [0, 1]
+assert solution.twoSum([-3, 4, 3, 90],0) == [0, 2]
+assert solution.twoSum([],0) == []
