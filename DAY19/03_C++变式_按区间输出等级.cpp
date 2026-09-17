45ac0a95e71edb9548fe2e6ca1f65c68c43410e2
CCY <changandaerduo@gmail.com>
2026-09-17 18:00:04 +0800

    区间输出等级

--- DAY19/03_C++变式_按区间输出等级.cpp
+++ DAY19/03_C++变式_按区间输出等级.cpp
@@ -1,1 +1,24 @@
 // DAY19 C++变式：读取分数，按三个区间输出等级；自己写三步分析、小程序和测试。
+#include<iostream>
+#include<cassert>
+
+int main(){
+
+    int score = 0;
+    std::cin >> score;
+
+    if(score >= 85){
+
+        std::cout << "A" << "\n";
+
+    }
+    else if(score >= 60){
+
+        std::cout << "B" << "\n";
+    }
+    else{
+
+        std::cout << "C" << "\n";
+    }
+
+}
