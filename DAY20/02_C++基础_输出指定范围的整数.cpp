1e81e26cf08d04ba3a4e6e580a9c0a2ecc041460
CCY <changandaerduo@gmail.com>
2026-09-18 11:14:59 +0800

    输出指定范围的整数

--- DAY20/02_C++基础_输出指定范围的整数.cpp
+++ DAY20/02_C++基础_输出指定范围的整数.cpp
@@ -1,1 +1,19 @@
 // DAY20 C++基础：用普通for循环输出指定范围；自己写三步分析、小程序和测试。
+
+#include<iostream>
+
+
+int main(){
+
+    int start;
+    int end;
+    std::cin >> start >> end;
+
+
+    for(int value = start; value<= end and value >= start; value = value +1){
+
+        std::cout << value << "\n";
+    }
+
+    return 0;
+}
