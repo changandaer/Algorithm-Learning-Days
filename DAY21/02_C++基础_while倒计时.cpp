663266ebb32f3e956b4840940771582964094f87
CCY <changandaerduo@gmail.com>
2026-09-21 20:50:26 +0800

    倒计时

--- DAY21/02_C++基础_while倒计时.cpp
+++ DAY21/02_C++基础_while倒计时.cpp
@@ -1,1 +1,17 @@
 // DAY21 C++基础：用while输出包含0的倒计时；输入输出见课程，三步、实现和测试自己写。
+
+#include<iostream>
+
+int main(){
+
+    int n = 0;
+    std::cin >> n;
+
+    while(n>=0){
+
+        std::cout << n <<"\n";
+        n -= 1;
+    }
+
+    return 0;
+}
