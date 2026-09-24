04090a6762af4e1fa26e0c21c39cf0d9354c150b
CCY <changandaerduo@gmail.com>
2026-09-24 21:51:12 +0800

    逐字符输出

--- DAY22/02_C++基础_字符串长度与逐字符输出.cpp
+++ DAY22/02_C++基础_字符串长度与逐字符输出.cpp
@@ -1,1 +1,17 @@
 // DAY22 C++基础：读取一个英文字符串，输出长度并逐字符输出；三步、小程序与测试自己写。
+#include<iostream>
+
+int main(){
+
+    std::string word;
+    std::cin>> word;
+    int len = word.size();
+    std::cout<< len <<"\n";
+    int i = 0;
+    while(i<len){
+        std::cout<< word[i]<<"\n";
+        i += 1;
+    }
+
+    return 0;
+}
