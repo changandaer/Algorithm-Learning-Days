e1d4ebf179ffe27dfc64098618003ffa87626c7e
CCY <changandaerduo@gmail.com>
2026-09-24 22:11:43 +0800

    统计指定字符

--- DAY22/03_C++变式_统计指定字符.cpp
+++ DAY22/03_C++变式_统计指定字符.cpp
@@ -1,1 +1,24 @@
 // DAY22 C++变式：读取字符串和目标字符，统计出现次数；准确输入输出见课程，自己实现。
+#include<iostream>
+#include<cassert>
+
+int main(){
+
+    std::string word;
+    char target;
+    std::cin >> word;
+    std::cin >> target;
+    int total = 0,len = word.size();
+
+    for(int i = 0;i<len;i+=1){
+
+        if(word[i] == target){
+            total += 1;
+        } 
+    }
+
+    std::cout<< total <<"\n";
+
+
+    return 0;
+}
